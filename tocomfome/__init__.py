1717101e1e5914db272308fa8d93d57dcf60c5fe
Paulo Ricardo Pereira Gomes <96184752+pauloricardopg@users.noreply.github.com>
2025-04-07 22:14:54 -0300

    Update __init__.py

--- tocomfome/__init__.py
+++ tocomfome/__init__.py
@@ -10,14 +10,14 @@ app.config['SECRET_KEY'] = '46ca2defb01cc7f0b122f36e6ca22r5p'
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
-# Corrige URL de PostgreSQL do Railway
+
 url = os.getenv("DATABASE_URL")
 if url and url.startswith("postgres://"):
     url = url.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = url or f'sqlite:///{os.path.join(base_dir, "tocomfome.db")}'
 
-# Extensões
+
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -25,7 +25,7 @@ login_manager.login_view = 'login'
 login_manager.login_message = 'Para acessar essa página você precisa estar logado'
 login_manager.login_message_category = 'alert-info'
 
-# Criação segura das tabelas
+
 from tocomfome import models
 with app.app_context():
     engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
@@ -36,5 +36,4 @@ with app.app_context():
     else:
         print("Base de dados já existente.")
 
-# Importa rotas
 from tocomfome import routes
